0192d2ed31d55384f87a340403f46746f3abefea
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-04-15 23:10:33 +0200

    added validation js
    added validation js

---
+++ src/js/authenticate.js
@@ -0,0 +1,48 @@
+const userNameContainer = document.querySelector("#username");
+const passwordContainer = document.querySelector("#password");
+
+const feedbackNameContainer = document.querySelector(".feedback-name");
+const feedbackPasswordContainer = document.querySelector(".feedback-password");
+
+const formContainer = document.querySelector(".login-form");
+
+//clear error message onchange and oninput
+userNameContainer.oninput = function() {
+    clearError(feedbackNameContainer, userNameContainer);
+}
+
+passwordContainer.oninput = function () {
+    clearError(feedbackPasswordContainer, passwordContainer);
+}
+
+function validate(event) {
+    // event.preventDefault()
+
+    let validLogin = true;
+
+    const name = userNameContainer.value;
+    const password = passwordContainer.value;
+
+    const validName = validateText(name);
+    const validPassword = validateText(password, 8);
+
+    if (!validName) {
+        event.preventDefault()
+        validLogin = false;
+        setError(feedbackNameContainer, userNameContainer, "Name should be atleast be a character");
+    }
+
+    if (!validPassword) {
+        event.preventDefault()
+        validLogin = false;
+        setError(feedbackPasswordContainer, passwordContainer, "Password should atleast be 8 characters");
+    }
+
+    // if (validLogin) {
+    //     formContainer.reset();
+    //     event.run();
+    // }
+
+}
+
+formContainer.addEventListener("submit", validate);
---
+++ src/js/components/validate.js
@@ -0,0 +1,25 @@
+function validateText (stringValue, minLenText = 1) {
+    if (stringValue.trim().length >= minLenText) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+function validateEmail (emailValue) {
+    const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const match = regEx.test(emailValue);
+    return match;   
+}
+
+function setError(noteContainer, inputContainer, errorMessage) {
+    noteContainer.innerHTML = `<div class="note">${errorMessage}</div>`;
+    inputContainer.style.borderColor = "red";
+}
+
+function clearError(noteContainer, inputContainer) {
+    noteContainer.innerHTML = "";
+    inputContainer.style.borderColor = "";
+}
