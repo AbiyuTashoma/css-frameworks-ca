c909755e61ca476c6b724dbe653f428262aa4900
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-04-24 21:36:31 +0200

    Local and session storage
    Local and session storage

--- src/js/json.js
+++ src/js/json.js
@@ -8,8 +8,37 @@ console.log("Original", myObject);
 
 const stringifyMyObj = JSON.stringify(myObject);
 
-console.log("Stringify", stringfyMyObj);
+console.log("Stringify", stringifyMyObj);
 
 const parseMyObj = JSON.parse(stringifyMyObj);
 
 console.log("Parsed", parseMyObj);
+
+//storing data
+localStorage.setItem('storeName', 'Ola');
+localStorage.setItem('storeName2', 'Olalala');
+
+//reteiving data
+const storeName = localStorage.getItem('storeName');
+console.log(storeName);
+
+//remove item
+localStorage.removeItem('storeName2');
+
+//clearing storage
+// localStorage.clear();
+
+//session storage
+const pet = {
+    type: 'Dog',
+    name: 'Lilje',
+}
+
+const petObj = JSON.stringify(pet);
+
+sessionStorage.setItem('petString', petObj);
+localStorage.setItem('localpet', petObj);
+
+const petLocal = localStorage.getItem('localpet');
+console.log(petLocal);
+console.log(JSON.parse(petLocal));
