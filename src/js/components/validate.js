a637ba19bc63c259ef30df56dbc6f6bbc137f948
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-04-24 20:50:39 +0200

    JSDocs
    Added JSDocs

--- src/js/components/validate.js
+++ src/js/components/validate.js
@@ -1,3 +1,17 @@
+/**
+ * Validates string value
+ * @param {string} stringValue value to be validated
+ * @param {number} [minLenText = 1] minimum length required for validation
+ * @returns {boolean} true if valid and false otherwise
+ * @example
+ * ```js
+ * // validate string length
+ * const inputValue = "name";
+ * const requiredLength = 2;
+ * const validity = validateText (inputValue, requiredLength);
+ * // Expected result true
+ * ```
+ */
 function validateText (stringValue, minLenText = 1) {
     if (stringValue.trim().length >= minLenText) {
         return true;
