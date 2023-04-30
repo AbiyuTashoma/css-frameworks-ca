b472e45fbfc05f639dfa73f0596d5cffee3d9155
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-04-30 10:57:30 +0200

    PUT PATCH DELETE methods
    PUT PATCH DELETE methods

--- src/js/js2-module4.js
+++ src/js/js2-module4.js
@@ -20,3 +20,38 @@ fetch(url, {
 })
 .then((response) => response.json())
 .then((json) => console.log(json));
+
+//PUT
+fetch(url + '/2', {
+    method: 'PUT',
+    body: JSON.stringify({
+        'id': 2,
+        'title': 'foo put',
+        'body': 'put body',
+        'userId': 2,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+//PATCH
+fetch(url + '/6', {
+    method: 'PATCH',
+    body: JSON.stringify({
+        'body': 'patched body',
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+//DELETE
+fetch(url + '/7', {
+    method: 'DELETE',
+})
+.then((response) => console.log(response));
