c471a55e8e612c9b7554be248ed0e9ff51b71745
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-04-30 23:06:27 +0200

    JWT
    JWT

--- src/js/js2-module4.js
+++ src/js/js2-module4.js
@@ -65,3 +65,90 @@ fetch(url + '/7', {
 
 // const ID = searchParameter.get('id');
 // searchParameter.set('country', 'Norway');
+
+//JWT Token
+console.log('JWT token');
+
+const BASE_URL = 'https://nf-api.onrender.com/api/v1/social';
+
+//Endpoints
+//Register: /auth/register
+//Login: /auth/login
+//Get: /posts
+
+//Register user
+async function registerUser (url, userData) {
+    const regOption = {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+    };
+
+    try {
+        const response = await fetch(url, regOption);
+        const json = await response.json();
+        console.log(json);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const registerURL = `${BASE_URL}/auth/register`;
+const data = {
+    "name": "profile_name",
+    "email": "profile_name@stud.noroff.no",
+    "password": "theusualcode", 
+}
+
+// registerUser (registerURL, data); registration complete
+
+//login and accessToken
+async function loginUser (url, loginCredential) {
+    const loginOption = {
+        method: 'POST',
+        body: JSON.stringify(loginCredential),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+    }
+    try {
+        const response = await fetch(url, loginOption);
+        const json = await response.json();
+        console.log(json);
+        localStorage.setItem('accessToken', json.accessToken);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const loginURL = `${BASE_URL}/auth/login`;
+const loginData = {
+    "email": "profile_name@stud.noroff.no",
+    "password": "theusualcode",
+}
+
+// loginUser (loginURL, loginData);
+
+//Request with JWT token
+async function getData (url) {
+    const accessToken = localStorage.getItem('accessToken');
+    const getOption = {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            Authorization: `Bearer ${accessToken}`,
+        },
+    }
+
+    const response = await fetch(url, getOption);
+    const json = await response.json();
+    console.log(json);
+}
+
+const getURL = `${BASE_URL}/posts`;
+
+getData (getURL);
