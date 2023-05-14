12f0afd63233ee9340f1e65329bfb0de6df5133e
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-05-14 20:07:14 +0200

    Added validation to post title
    Added validation to post title

--- src/js/post.js
+++ src/js/post.js
@@ -3,29 +3,10 @@ const postBodyContainer = document.querySelector('#post-body');
 const postMediaContainer = document.querySelector('#post-media');
 const postBtnContainer = document.querySelector('#post-btn');
 const postFormContainer = document.querySelector('#createContent');
+const noteTitleContainer = document.querySelector('.note-title');
 
 // const BASE_URL = 'https://nf-api.onrender.com/api/v1/social';
 const postURL = BASE_URL + '/posts';
-
-// const postTitle = postTitleContainer.value;
-// const postBody = postBodyContainer.value;
-// const postMedia = postMediaContainer.value;
-
-// const postData = {
-//     "title": `${postTitle}`,
-//     "body": `${postBody}`,
-//     "tags": ['SoMeABT'],
-//     "media": `${postMedia}`
-// };
-
-// const postOption = {
-//     method: 'POST',
-//     body: JSON.stringify(postData),
-//     headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//         Authorization: `Bearer ${accessToken}`,
-//     },
-// }
 
 async function postPost(event) {
     event.preventDefault();
@@ -34,32 +15,44 @@ async function postPost(event) {
     const postBody = postBodyContainer.value;
     const postMedia = postMediaContainer.value;
     
-    const postData = {
-        "title": `${postTitle}`,
-        "body": `${postBody}`,
-        "tags": ['SoMeABT'],
-        "media": `${postMedia}`
-    };
-    
-    const postOption = {
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-            Authorization: `Bearer ${accessToken}`,
-        },
+    const validTitle = validateText(postTitle, 1, 100);
+
+    if (!validTitle) {
+        setFeedback(noteTitleContainer, postTitleContainer, "Title is required", "text-danger");
     }
 
-    try {
-        const response = await fetch (postURL, postOption);
-        const json = await response.json();
-        console.log(json);
-    }
+    else {
+        const postData = {
+            "title": `${postTitle}`,
+            "body": `${postBody}`,
+            "tags": ['SoMeABT'],
+            "media": `${postMedia}`
+        };
+        
+        const postOption = {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        }
 
-    catch(error) {
-        console.log(error);
+        try {
+            const response = await fetch (postURL, postOption);
+            const json = await response.json();
+            console.log(json);
+        }
+
+        catch(error) {
+            console.log(error);
+        }
     }
     
+}
+
+postTitleContainer.oninput = function() {
+    clearFeedback(noteTitleContainer, postTitleContainer);
 }
 
 postFormContainer.addEventListener('submit', postPost);
