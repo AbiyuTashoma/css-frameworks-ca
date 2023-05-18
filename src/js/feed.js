85743388ca12e9d6c45a662151e090ce0d8ceb4b
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-05-18 19:53:27 +0200

    commented out feed and create post from branch
    commented out feed and create post from branch

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,32 +1,30 @@
-const BASE_URL = 'https://nf-api.onrender.com/api/v1/social';
-const feedURL = BASE_URL + '/posts';
-const feedLimit = feedURL + '?limit=20';
-const feedTag = feedURL + '?_tag=SoMeABT';
+// const BASE_URL = 'https://nf-api.onrender.com/api/v1/social';
+// const feedURL = BASE_URL + '/posts';
+// const feedLimit = feedURL + '?limit=20';
+// const feedTag = feedURL + '?_tag=SoMeABT';
 
-const feedContainer = document.querySelector('.feed-content');
+// const feedContainer = document.querySelector('.feed-content');
 
-//GET
-const accessToken = localStorage.getItem('accessToken');
-// console.log(accessToken);
+// const accessToken = localStorage.getItem('accessToken');
 
-const feedOption = {
-    method: 'GET',
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        Authorization: `Bearer ${accessToken}`,
-    },
-};
+// const feedOption = {
+//     method: 'GET',
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//         Authorization: `Bearer ${accessToken}`,
+//     },
+// };
 
-async function feed() {
-    const apiJson = await apiRequest(feedTag, feedOption);
-    if (apiJson['json']) {
-        feedContainer.innerHTML = createHtml (apiJson['json']);
-        console.log('async', apiJson['json']);
-    }
+// async function feed() {
+//     const apiJson = await apiRequest(feedTag, feedOption);
+//     if (apiJson['json']) {
+//         feedContainer.innerHTML = createHtml (apiJson['json']);
+//         console.log('async', apiJson['json']);
+//     }
 
-    if (apiJson['error']) {
-        console.log('error', apiJson['error']);
-    }
-}
+//     if (apiJson['error']) {
+//         console.log('error', apiJson['error']);
+//     }
+// }
 
-feed();
+// feed();
