9200d185ee8e7a85b64de62bae43a4c68e348005
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-06-29 20:42:51 +0200

    in progress
    in progress

--- src/js/components/createhtml.js
+++ src/js/components/createhtml.js
@@ -52,13 +52,19 @@ function createHtml(jasonArray, postAuthor, cnt, btnState = 'disabled') {
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                     <div>
-                        <svg viewBox="0 100 1024 1024" height="20" width="25" class="svg-fill" fill="green" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                            <g id="SVGRepo_iconCarrier">
+                        <svg viewBox="0 0 1024 1024" class="svg-fill" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g>
                                 <path d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z"></path>
                             </g>
                         </svg>
-                        <img src="src/image/icons/comment-dots.png" class="post-icon" alt="comment">
-                        <img src="src/image/icons/share.png" class="post-icon" alt="share">
+                        <svg viewBox="0 0 24 24" class="svg-fill" xmlns="http://www.w3.org/2000/svg">
+                            <g><defs><style>.cls-1{stroke:rgb(1, 84, 158);stroke-miterlimit:10;stroke-width:1.91px;}</style></defs><path class="cls-1" d="M1.5,5.3v9.54a3.82,3.82,0,0,0,3.82,3.82H7.23v2.86L13,18.66h5.73a3.82,3.82,0,0,0,3.82-3.82V5.3a3.82,3.82,0,0,0-3.82-3.82H5.32A3.82,3.82,0,0,0,1.5,5.3Z"></path><line class="cls-1" x1="15.82" y1="10.07" x2="17.73" y2="10.07"></line><line class="cls-1" x1="11.05" y1="10.07" x2="12.95" y2="10.07"></line><line class="cls-1" x1="6.27" y1="10.07" x2="8.18" y2="10.07"></line></g>
+                        </svg>
+                        <svg viewBox="0 0 1024 1024" class="svg-fill" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g>
+                                <path d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z"></path>
+                            </g>
+                        </svg>
                     </div>
                     <div class="dropdown">
                         <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
